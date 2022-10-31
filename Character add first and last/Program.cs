505e32ea5838ef2bdc37f01e09550d3944923cc9
Rintu <Rintu@VINDUJA>
2022-10-31 16:28:41 +0530

    Add project files.

---
+++ Character add first and last/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Character_add_first_and_last_17
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("eneter any string ");
+            string str=Console.ReadLine();
+
+            var s = str.Substring(0,1);
+            Console.WriteLine(s + str + s);
+            Console.ReadLine();
+        }
+    }
+}
